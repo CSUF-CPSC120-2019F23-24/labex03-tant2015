39c2d5bf6f2eff24c205cd34b4e4cbd97c086c75
hnguyen1186 <hnguyen1186@cs104u-4a.acad.fullerton.edu>
2019-09-07 13:27:02 -0700

    Done with prob01

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -6,9 +6,9 @@ int main()
 {
   // TODO#1: try changing these values to others found in the ASCII character set
   // The class Quick Reference Guide contains this chart on page 5
-  char my_char1 = 67;   // decimal
-  char my_char2 = 0x43; // hex
-  char my_char3 = 'C';  // character
+  char my_char1 = 97;   // decimal
+  char my_char2 = 0x3C; // hex
+  char my_char3 = 'u';  // character
 
   std::cout << "Char1: " << my_char1 << std::endl;
   std::cout << "Char2: " << my_char2 << std::endl;
@@ -20,26 +20,34 @@ int main()
   // add 3 more cout statements that all print the same thing - a plus sign
   // however, use a different method to do so for each cout statement
   // hint: use the ASCII character set
+
+  char my_char4 = 43; // decimal
+  char my_char5 = 0x2B;
+  char my_char6 = '+';
+
   std::cout << "+" << std::endl;
+  std::cout << my_char4 << std::endl;
+  std::cout << my_char5 << std::endl;
+  std::cout << my_char6  << std::endl;
 
   std::cout << std::endl; // blank line to separate the different exercises
 
   // TODO#3: update the following statement to include escape characters
   // note that clang does not require an escape character on the single quote
   // but many other compilers do require it
-  std::cout << "'Y'" << std::endl;
+  std::cout << "\'Y\'" << " I am going to say \"hello\"" << std::endl;
 
   std::cout << std::endl; // blank line to separate the different exercises
 
   // TODO#4: try changing the variable types and the data types in the
   // statements below to see what is returned by the sizeof operator
-  bool my_bool = 1;
+  double my_bool = 2;
   std::cout << my_bool << ' ' << sizeof(my_bool) << '\n';
-  short my_int = 1;
+  bool my_int = 3;
   std::cout << my_int << ' ' << sizeof(my_int) << '\n';
 
-  double amount;
-  std::cout << "A float is stored in " << sizeof(float) << " bytes\n";
+  short amount;
+  std::cout << "A float is stored in " << sizeof(short) << " bytes\n";
   std::cout << "The variable \"amount\" is stored in " << sizeof(amount) << " bytes\n";
 
   return 0;
